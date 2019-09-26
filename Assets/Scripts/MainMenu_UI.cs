061ecbd0ef734faa85e0e5b8d282f897bf773982
Daniel Gleason <damianscape@gmail.com>
2019-09-25 20:26:57 -0400

    added textmesh pro, and very basic mainmenu script.

---
+++ Assets/Scripts/MainMenu_UI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UI : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TestMap");
+    }
+
+}
