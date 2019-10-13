cc91c3b38848e8df975c06dd402a5ef60f190215
Daniel Gleason <damianscape@gmail.com>
2019-10-13 16:16:30 -0400

    added script to fade between level loads.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class LevelLoader : MonoBehaviour
+{
+    public string LevelName;
+    public GameObject FaderObject;
+    private Animator animController;
+
+    void Start()
+    {
+        animController = FaderObject.GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        animController.SetTrigger("FadeToBlack");
+        StartCoroutine(WaitForFadeEnd());
+    }
+
+    IEnumerator WaitForFadeEnd()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(LevelName);
+    }
+
+}
