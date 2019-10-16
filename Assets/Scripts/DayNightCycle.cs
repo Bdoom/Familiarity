618d5a13da145fbf9de3892a5ca2eeefc4472c67
Daniel Gleason <damianscape@gmail.com>
2019-10-16 18:16:37 -0400

    cleaning up some code in DayNightCycle.cs

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -6,19 +6,11 @@ using UnityEngine;
 public class DayNightCycle : MonoBehaviour
 {
     public Image Fader;
-    float currentTime = 0;
-    float durationSeconds = 20 * 60;
+    public float durationSeconds = 20 * 60;
 
     void Update()
     {
-
-
-        currentTime += Time.deltaTime;
-        //float lerpTimer = Mathf.Lerp(0, 1, currentTime / timeToCycle);
-        float lerpTimer = Mathf.PingPong(Time.time / durationSeconds, 1);
-        Debug.Log(lerpTimer);
-        Fader.color = new Vector4(0, 0, 0, lerpTimer);
-
-
+        float dayNightTimer = Mathf.PingPong(Time.time / durationSeconds, 1);
+        Fader.color = new Vector4(0, 0, 0, dayNightTimer);
     }
 }
